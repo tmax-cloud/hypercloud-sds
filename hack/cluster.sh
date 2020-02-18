eb03d36f8fda5211abd07761df00a2e99c767321
jaeyeon_kim <jaeyeon_kim@tmax.co.kr>
2020-02-18 12:36:09 +0900

    [Enh] clusterUp 시 input argument 추가
    - os version, node count, k8s version 을 input argument로
      받을 수 있도록 수정
    - gitlab ci 에 extends 적용하여 여러 버전 테스트하도록 수정

--- hack/cluster.sh
+++ hack/cluster.sh
@@ -1,9 +1,65 @@
 #!/bin/bash
 
-# shellcheck source=common.sh
-. "$(dirname "$0")/common.sh"
+# Exit script when any commands failed
+set -eo pipefail
 
+#####################
+# define common variables
+srcDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../"
 multinodeK8sDir="${srcDir}/hack/k8s-vagrant-multi-node"
+clusterConfigDir="$multinodeK8sDir"/.created-cluster-config
+
+#####################
+# util functions
+function kubectl_wait_avail() {
+  namespace=$1
+  waitFor=$2
+  timeoutSecond=$3
+
+  echo "Waiting for available ${waitFor}..."
+
+  for ((seconds = 0; seconds <= timeoutSecond; seconds = seconds + 1)); do
+    if kubectl describe -n "${namespace}" "${waitFor}" &>/dev/null; then break; fi
+    sleep 1
+  done
+
+  kubectl wait -n "${namespace}" --for=condition=available "${waitFor}" --timeout="${timeoutSecond}"s
+}
+
+function kubectl_wait_delete() {
+  namespace=$1
+  waitFor=$2
+  timeoutSecond=$3
+
+  echo "Waiting for deleting ${waitFor}..."
+
+  for ((seconds = 0; seconds <= timeoutSecond; seconds = seconds + 1)); do
+    if ! kubectl describe -n "${namespace}" "${waitFor}" &>/dev/null; then break; fi
+    sleep 1
+  done
+}
+
+function print_red() {
+  msg=$1
+
+  echo -e "\e[0;31;47m${msg}\e[0m"
+}
+
+function sourceConfigFromFile() {
+  if [ ! -f "$clusterConfigDir" ]; then
+    echo "There isn't any config file in $clusterConfigDir"
+    exit 1
+  fi
+
+  source "$clusterConfigDir"
+}
+
+function checkWhetherClusterIsRunning() {
+  if [ -f "$clusterConfigDir" ]; then
+    echo "There is running cluster already in $clusterConfigDir"
+    exit 1
+  fi
+}
 
 function waitMinikubeSsh() {
   local tries=100
@@ -17,13 +73,39 @@ function waitMinikubeSsh() {
   echo ERROR: ssh did not come up >&2
   exit 1
 }
+#####################
+# main features
+function clusterUp() {
+  checkWhetherClusterIsRunning
+
+  local nodeCount=${NODE_COUNT:?"need env NODE_COUNT"}
+  local os=${BOX_OS:?"need env BOX_OS"}
+  local k8sVersion=${KUBERNETES_VERSION:?"need env KUBERNETES_VERSION"}
+
+  # temporarily save config for deleting later
+  echo "NODE_COUNT=$nodeCount BOX_OS=$os KUBERNETES_VERSION=$k8sVersion" >"$clusterConfigDir"
+
+  echo "create $os-$nodeCount-worker-node-cluster with k8s version : $k8sVersion"
+
+  DISK_COUNT=2 DISK_SIZE_GB=5 make --directory "${multinodeK8sDir}" up -j2
+  print_red "========================== cluster created =========================="
+  echo "However, you may need to wait a few seconds until nodes are ready"
+  kubectl get nodes
+}
+
+function clusterClean() {
+  sourceConfigFromFile
+
+  NODE_COUNT="$NODE_COUNT" make --directory "${multinodeK8sDir}" clean -j"$(nproc)"
+
+  rm "$clusterConfigDir"
+  echo "The cluster-config file deleted in $clusterConfigDir"
+}
 
 function minikubeUp() {
   # TODO: minikube version
   # TODO: multinode cluster
-  # TODO: minikube profile
-
-  minikube start
+  minikube start --vm-driver=virtualbox
   waitMinikubeSsh
 
   # Rook에서 사용할 디렉토리를 마운트
@@ -37,39 +119,26 @@ function minikubeClean() {
   minikube delete
 }
 
-function clusterUp() {
-  # TODO vagrant global-status --prune check or ps -ef | vagrant check
-  DISK_COUNT=2 DISK_SIZE_GB=5 NODE_COUNT=3 make --directory "${multinodeK8sDir}" up -j2
-  print_red "========================== cluster created =========================="
-  echo "However, you may need to wait some seconds until nodes are ready"
-}
-
-function clusterClean() {
-  DISK_COUNT=2 DISK_SIZE_GB=5 NODE_COUNT=3 make --directory "${multinodeK8sDir}" clean -j"$(nproc)"
-}
-
-function main() {
-  case "${1:-}" in
-  minikubeUp)
-    minikubeUp
-    ;;
-  minikubeClean)
-    minikubeClean
-    ;;
-  clusterUp)
-    clusterUp
-    ;;
-  clusterClean)
-    clusterClean
-    ;;
-  *)
-    echo "usage:" >&2
-    echo "  $0 minikubeUp" >&2
-    echo "  $0 minikubeClean" >&2
-    echo "  $0 clusterUp" >&2
-    echo "  $0 clusterClean" >&2
-    ;;
-  esac
-}
-
-main "$1"
+#####################
+# main logic
+case "$1" in
+minikubeUp)
+  minikubeUp
+  ;;
+minikubeClean)
+  minikubeClean
+  ;;
+clusterUp)
+  clusterUp
+  ;;
+clusterClean)
+  clusterClean
+  ;;
+*)
+  echo "usage:" >&2
+  echo "  $0 minikubeUp" >&2
+  echo "  $0 minikubeClean" >&2
+  echo "  BOX_OS=fedora NODE_COUNT=3 KUBERNETES_VERSION=1.15.3 $0 clusterUp" >&2
+  echo "  $0 clusterClean" >&2
+  ;;
+esac
