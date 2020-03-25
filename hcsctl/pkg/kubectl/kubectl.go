1981388eb05cbe322ab1d219dbdc6272c5ed8005
jaeyeon_kim <jaeyeon_kim@tmax.co.kr>
2020-03-25 15:25:22 +0900

    [Enh] kubectl cmd 에 timeout 추가
    - stuck 이 걸리는 상황을 위하여 kubectl 커맨드에 15 분의
      rough 한 timeout 을 설정

--- hcsctl/pkg/kubectl/kubectl.go
+++ hcsctl/pkg/kubectl/kubectl.go
@@ -1,12 +1,21 @@
 package kubectl
 
 import (
+	"context"
 	"io"
 	"os/exec"
+	"time"
+)
+
+const (
+	CMD_TIMEOUT = 15 * time.Minute
 )
 
 func Run(stdout, stderr io.Writer, arg ...string) error {
-	cmd := exec.Command("kubectl", arg...)
+	ctx, cancel:= context.WithTimeout(context.Background(), CMD_TIMEOUT)
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, "kubectl", arg...)
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
 	err := cmd.Run()
