1d6477bf36567554fce126dc5194f3b2dfd9ab0e
sophal <tech.sophal@gmail.com>
2020-03-30 11:17:08 +0900

    [skip ci] kubespray_offline: Update scripts - shellcheck PASS

--- docs/kubespray_offline_installation/download_binary.sh
+++ docs/kubespray_offline_installation/download_binary.sh
@@ -18,25 +18,25 @@ echo "Download location: ${BINARY_DIR}"
 
 # download kubelet kubectl kubeadm
 ARCH="amd64"
-VERSION=$(grep ^kube_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+VERSION=$(grep ^kube_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 LOC="storage.googleapis.com/kubernetes-release/release/${VERSION}/bin/linux/${ARCH}"
 for FILE in "kubelet" "kubectl" "kubeadm"; do
 	echo -e "\n[Download] ${FILE}:${VERSION}"
-	mkdir -p ${BINARY_DIR}/${LOC}
+	mkdir -p ${BINARY_DIR}/"${LOC}"
 	TARGET="${BINARY_DIR}/${LOC}/${FILE}"
-	wget -q -O ${TARGET} "https://${LOC}/${FILE}"
-	ls -lh ${TARGET}
-	CHKSUM=$(sha256sum ${TARGET} | awk '{print $1}')
+	wget -q -O "${TARGET}" "https://${LOC}/${FILE}"
+	ls -lh "${TARGET}"
+	CHKSUM=$(sha256sum "${TARGET}" | awk '{print $1}')
 	echo -e "[SHA256SUM] ${FILE}:${VERSION} - ${CHKSUM}"
 	case ${FILE} in
 		"kubelet" )
-			sed -i "s/^\(\s*kubelet_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+			sed -i "s/^\(\s*kubelet_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 			;;
 		"kubectl" )
-			sed -i "s/^\(\s*kubectl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+			sed -i "s/^\(\s*kubectl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 			;;
 		"kubeadm" )
-			sed -i "s/^\(\s*kubeadm_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+			sed -i "s/^\(\s*kubeadm_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 			;;
 		* )
 			echo "[ERROR] Not match binary file: ${FILE}"
@@ -47,63 +47,63 @@ for FILE in "kubelet" "kubectl" "kubeadm"; do
 done
 
 # download etcd binary
-VERSION=$(grep ^etcd_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+VERSION=$(grep ^etcd_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 LOC="github.com/coreos/etcd/releases/download/${VERSION}"
 FILE="etcd-${VERSION}-linux-${ARCH}.tar.gz"
 echo -e "\n[Download] ${FILE}:${VERSION}"
-mkdir -p ${BINARY_DIR}/${LOC}
+mkdir -p "${BINARY_DIR}"/"${LOC}"
 TARGET="${BINARY_DIR}/${LOC}/${FILE}"
-wget -q -O ${TARGET} "https://${LOC}/${FILE}"
-ls -lh ${TARGET}
-CHKSUM=$(sha256sum ${TARGET} | awk '{print $1}')
+wget -q -O "${TARGET}" "https://${LOC}/${FILE}"
+ls -lh "${TARGET}"
+CHKSUM=$(sha256sum "${TARGET}" | awk '{print $1}')
 echo -e "[SHA256SUM] ${FILE}:${VERSION} - ${CHKSUM}"
-sed -i "s/^\(\s*etcd_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+sed -i "s/^\(\s*etcd_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 echo "------------------------------------------------------------"
 
 # download cni-plugins
-VERSION=$(grep ^cni_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+VERSION=$(grep ^cni_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 LOC="github.com/containernetworking/plugins/releases/download/${VERSION}"
 FILE="cni-plugins-linux-${ARCH}-${VERSION}.tgz"
 echo -e "\n[Download] ${FILE}:${VERSION}"
-mkdir -p ${BINARY_DIR}/${LOC}
+mkdir -p ${BINARY_DIR}/"${LOC}"
 TARGET="${BINARY_DIR}/${LOC}/${FILE}"
-wget -q -O ${TARGET} "https://${LOC}/${FILE}"
-ls -lh ${TARGET}
-CHKSUM=$(sha256sum ${TARGET} | awk '{print $1}')
+wget -q -O "${TARGET}" "https://${LOC}/${FILE}"
+ls -lh "${TARGET}"
+CHKSUM=$(sha256sum "${TARGET}" | awk '{print $1}')
 echo -e "[SHA256SUM] ${FILE}:${VERSION} - ${CHKSUM}"
-sed -i "s/^\(\s*cni_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+sed -i "s/^\(\s*cni_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 echo "------------------------------------------------------------"
 
 # download calicoctl
-VERSION=$(grep ^calico_ctl_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+VERSION=$(grep ^calico_ctl_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 LOC="github.com/projectcalico/calicoctl/releases/download/${VERSION}"
 FILE="calicoctl-linux-${ARCH}"
 echo -e "\n[Download] ${FILE}:${VERSION}"
-mkdir -p ${BINARY_DIR}/${LOC}
+mkdir -p ${BINARY_DIR}/"${LOC}"
 TARGET="${BINARY_DIR}/${LOC}/${FILE}"
-wget -q -O ${TARGET} "https://${LOC}/${FILE}"
-ls -lh ${TARGET}
-CHKSUM=$(sha256sum ${TARGET} | awk '{print $1}')
+wget -q -O $"{TARGET}" "https://${LOC}/${FILE}"
+ls -lh $"{TARGET}"
+CHKSUM=$(sha256sum "${TARGET}" | awk '{print $1}')
 echo -e "[SHA256SUM] ${FILE}:${VERSION} - ${CHKSUM}"
-sed -i "s/^\(\s*calicoctl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+sed -i "s/^\(\s*calicoctl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 echo "------------------------------------------------------------"
 
 # download crictl
 crictl_versions="v1.15.0 v1.16.1 v1.17.0"
-kube_version=$(grep ^kube_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+kube_version=$(grep ^kube_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 major_version=${kube_version::-2} # v1.15.3 -> v1.15
 for VERSION in ${crictl_versions}; do
 	if [[ ${VERSION} =~ ${major_version} ]]; then
 		LOC="github.com/kubernetes-sigs/cri-tools/releases/download/${VERSION}"
 		FILE="crictl-${VERSION}-linux-${ARCH}.tar.gz"
 		echo -e "\n[Download] ${FILE}"
-		mkdir -p ${BINARY_DIR}/${LOC}
+		mkdir -p ${BINARY_DIR}/"${LOC}"
 		TARGET="${BINARY_DIR}/${LOC}/${FILE}"
-		wget -q -O ${TARGET} "https://${LOC}/${FILE}"
-		ls -lh ${TARGET}
-		CHKSUM=$(sha256sum ${TARGET} | awk '{print $1}')
+		wget -q -O "${TARGET}" "https://${LOC}/${FILE}"
+		ls -lh "${TARGET}"
+		CHKSUM=$(sha256sum "${TARGET}" | awk '{print $1}')
 		echo -e "[SHA256SUM] ${FILE}:${VERSION} - ${CHKSUM}"
-		sed -i "s/^\(\s*crictl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" ${CLUSTER_YML}
+		sed -i "s/^\(\s*crictl_binary_checksum\s*:\s*\).*/\1${CHKSUM}/" "${CLUSTER_YML}"
 		echo "------------------------------------------------------------"
 		break
 	fi
--- docs/kubespray_offline_installation/push_docker_image.sh
+++ docs/kubespray_offline_installation/push_docker_image.sh
@@ -15,7 +15,7 @@ REG_URL="$1"
 CLUSTER_YML="$2"
 IMAGE_LIST=()
 
-status_code=$(curl -I -k -s ${REG_URL} | head -n 1 | cut -d ' ' -f 2)
+status_code=$(curl -I -k -s "${REG_URL}" | head -n 1 | cut -d ' ' -f 2)
 if [[ "$status_code" != "200" ]]; then
 	echo "[ERROR] Docker registry (${REG_URL}) is not running."
 	exit 1
@@ -23,41 +23,41 @@ fi
 
 set -eo pipefail
 # Kubespary-based container image
-TAG=$(grep ^nginx_image_tag ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^nginx_image_tag "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("docker.io/library/nginx:${TAG}")
 
-TAG=$(grep ^calico_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^calico_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("docker.io/calico/node:${TAG}")
 
-TAG=$(grep ^calico_cni_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^calico_cni_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("docker.io/calico/cni:${TAG}")
 
-TAG=$(grep ^calico_policy_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^calico_policy_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("docker.io/calico/kube-controllers:${TAG}")
 
-TAG=$(grep ^coredns_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^coredns_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("docker.io/coredns/coredns:${TAG}")
 
-TAG=$(grep ^nodelocaldns_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^nodelocaldns_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("gcr.io/google-containers/k8s-dns-node-cache:${TAG}")
 
-TAG=$(grep ^kube_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^kube_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("gcr.io/google-containers/kube-proxy:${TAG}")
 IMAGE_LIST+=("gcr.io/google-containers/kube-apiserver:${TAG}")
 IMAGE_LIST+=("gcr.io/google-containers/kube-scheduler:${TAG}")
 IMAGE_LIST+=("gcr.io/google-containers/kube-controller-manager:${TAG}")
 
-TAG=$(grep ^dnsautoscaler_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^dnsautoscaler_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("gcr.io/google-containers/cluster-proportional-autoscaler-amd64:${TAG}")
 
-TAG=$(grep ^dashboard_image_tag ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^dashboard_image_tag "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("gcr.io/google_containers/kubernetes-dashboard-amd64:${TAG}")
 
-TAG=$(grep ^pod_infra_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^pod_infra_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("gcr.io/google-containers/pause:${TAG}")
 IMAGE_LIST+=("gcr.io/google_containers/pause-amd64:${TAG}")
 
-TAG=$(grep ^etcd_version ${CLUSTER_YML} | cut -d ' ' -f2 | tr -d '"')
+TAG=$(grep ^etcd_version "${CLUSTER_YML}" | cut -d ' ' -f2 | tr -d '"')
 IMAGE_LIST+=("quay.io/coreos/etcd:${TAG}")
 
 
@@ -65,14 +65,14 @@ for image in "${IMAGE_LIST[@]}"
 do
 	echo ""
 	echo -e "\n[PULL]<- ${image}"
-	docker pull ${image}
+	docker pull "${image}"
 
-	ORI_URL=$(echo ${image} | cut -d '/' -f1)
+	ORI_URL=$(echo "${image}" | cut -d '/' -f1)
 	NEW_IMAGE="${image//${ORI_URL}/${REG_URL}}"
 
 	echo -e "\n[PUSH]-> ${NEW_IMAGE}"
-	docker tag ${image} ${NEW_IMAGE}
-	docker push ${NEW_IMAGE}
+	docker tag "${image}" "${NEW_IMAGE}"
+	docker push "${NEW_IMAGE}"
 done
 exit
 
